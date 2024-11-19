deede324f1957847bd5dcf2ae05366e8a2e5593c
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-11-19 23:15:05 +0900

    initial

---
+++ src/main/java/home/bangbanggoodgood/BangbanggoodgoodApplication.java
@@ -0,0 +1,13 @@
+package home.bangbanggoodgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbanggoodgoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangbanggoodgoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/bangbanggoodgood/BangbanggoodgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package home.bangbanggoodgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangbanggoodgoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
